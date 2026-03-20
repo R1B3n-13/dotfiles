b3da49b2bc7620b2a013e36623f40aefa4b8ec29
Sadik Al Barid <70191239+R1B3n-13@users.noreply.github.com>
2026-03-20 19:37:03 +0600

    Enhance WezTerm config with tabs and colors (win-dots)
    Refactor WezTerm configuration for better organization and added custom tab colors.

--- win-dots/wezterm/wezterm.lua
+++ win-dots/wezterm/wezterm.lua
@@ -1,10 +1,44 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+-- Default shell
 if wezterm.target_triple:find("windows") then
   config.default_prog = { 'pwsh.exe', '-NoLogo' }
-  config.color_scheme = 'Rosé Pine (Gogh)'
-  config.default_cursor_style = "BlinkingBar"
 end
+
+-- Colorscheme
+config.color_scheme = 'Rosé Pine (Gogh)'
+
+-- Cursor
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 500  -- in ms
+
+-- Tabs
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 24
+
+-- Custom tab colors
+config.colors = {
+  tab_bar = {
+    background = '#1a1b26',
+    active_tab = {
+      bg_color = '#16161e',
+      fg_color = '#c0caf5',
+    },
+    inactive_tab = {
+      bg_color = '#2a2b3d',
+      fg_color = '#a9b1d6',
+    },
+    inactive_tab_hover = {
+      bg_color = '#3b3d5c',
+      fg_color = '#c0caf5',
+    },
+    new_tab = {
+      bg_color = '#1a1b26',
+      fg_color = '#a9b1d6',
+    },
+  },
+}
 
 return config
