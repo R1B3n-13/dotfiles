ed509dbcea09fd396d8f014cc3fd020d552d2344
Sadik Al Barid <70191239+R1B3n-13@users.noreply.github.com>
2026-03-20 20:30:09 +0600

    add delay before initializing oh-my-posh (win-dots)

--- win-dots/powershell/Microsoft.PowerShell_profile.ps1
+++ win-dots/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\sadik\.config\komorebi'
 if ($PSVersionTable.PSVersion.Major -ge 7) {
+    Start-Sleep -Milliseconds 50
     oh-my-posh init pwsh --config "1_shell" | Invoke-Expression
 }
