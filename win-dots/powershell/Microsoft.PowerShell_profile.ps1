9a032eeeb46e8323aca7c8e577047eada0a3528a
Sadik Al Barid <70191239+R1B3n-13@users.noreply.github.com>
2026-03-20 21:10:12 +0600

    adjust sleep duration in PowerShell profile (win-dots)
    Increased sleep duration from 50 to 100 milliseconds.

--- win-dots/powershell/Microsoft.PowerShell_profile.ps1
+++ win-dots/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\sadik\.config\komorebi'
 if ($PSVersionTable.PSVersion.Major -ge 7) {
-    Start-Sleep -Milliseconds 50
+    Start-Sleep -Milliseconds 100
     oh-my-posh init pwsh --config "1_shell" | Invoke-Expression
 }
