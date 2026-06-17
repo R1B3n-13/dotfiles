fbf13e55f757a31aa146f78e857cbe78a7ea020b
Sadik Al Barid <sadik.barid@therapservices.net>
2026-06-17 15:13:22 +0600

    add more functionality to mini.ai

--- nvim/lua/plugins/arborist.lua
+++ nvim/lua/plugins/arborist.lua
@@ -1,8 +1,8 @@
 return {
 	"arborist-ts/arborist.nvim",
 	config = function()
-		local config = require("arborist")
-		config.setup({
+		local arborist = require("arborist")
+		arborist.setup({
 			prefer_wasm = false,
 			install_popular = false,
 		})
--- nvim/lua/plugins/mini-ai.lua
+++ nvim/lua/plugins/mini-ai.lua
@@ -2,12 +2,21 @@ return {
 	"echasnovski/mini.ai",
 	event = { "BufReadPost", "BufNewFile" },
 	config = function()
-		local config = require("mini.ai")
-		config.setup({
-			n_lines = 500, -- How far to look for a text object
+		local ai = require("mini.ai")
+		local ts = ai.gen_spec.treesitter
+		ai.setup({
+			n_lines = 500,
 			custom_textobjects = {
-				f = config.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
-				c = config.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+				f = ts({ a = "@function.outer", i = "@function.inner" }),
+				c = ts({ a = "@class.outer", i = "@class.inner" }),
+				i = ts({ a = "@conditional.outer", i = "@conditional.inner" }),
+				l = ts({ a = "@loop.outer", i = "@loop.inner" }),
+				p = ts({ a = "@parameter.outer", i = "@parameter.inner" }),
+				k = ts({ a = "@call.outer", i = "@call.inner" }),
+				b = ts({ a = "@block.outer", i = "@block.inner" }),
+				r = ts({ a = "@return.outer", i = "@return.inner" }),
+				["="] = ts({ a = "@assignment.outer", i = "@assignment.rhs" }),
+				["/"] = ts({ a = "@comment.outer", i = "@comment.inner" }),
 			},
 		})
 	end,
