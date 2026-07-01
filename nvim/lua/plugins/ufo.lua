ac615fe97b33731e0de895877e87257d454e65ad
Sadik Al Barid <sadik.barid@therapservices.net>
2026-07-01 15:34:17 +0600

    customize ufo.nvim

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -2,10 +2,45 @@ return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = { "kevinhwang91/promise-async" },
 	config = function()
+		local handler = function(virtText, lnum, endLnum, width, truncate)
+			local newVirtText = {}
+			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+			local sufWidth = vim.fn.strdisplaywidth(suffix)
+			local targetWidth = width - sufWidth
+			local curWidth = 0
+			for _, chunk in ipairs(virtText) do
+				local chunkText = chunk[1]
+				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+				if targetWidth > curWidth + chunkWidth then
+					table.insert(newVirtText, chunk)
+				else
+					chunkText = truncate(chunkText, targetWidth - curWidth)
+					local hlGroup = chunk[2]
+					table.insert(newVirtText, { chunkText, hlGroup })
+					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if curWidth + chunkWidth < targetWidth then
+						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+					end
+					break
+				end
+				curWidth = curWidth + chunkWidth
+			end
+			table.insert(newVirtText, { suffix, "MoreMsg" })
+			return newVirtText
+		end
+
 		require("ufo").setup({
+			fold_virt_text_handler = handler,
 			provider_selector = function(_, _, _)
 				return { "treesitter", "indent" }
 			end,
+			preview = {
+				win_config = {
+					border = "rounded",
+					winhighlight = "Normal:Normal",
+					winblend = 0,
+				},
+			},
 		})
 
 		-- Options
@@ -14,5 +49,12 @@ return {
 		vim.o.foldlevel = 99
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
+
+		vim.keymap.set("n", "J", function()
+			local winid = require("ufo").peekFoldedLinesUnderCursor()
+			if winid then
+				require("ufo").peekFoldedLinesUnderCursor(true)
+			end
+		end, { desc = "Peek fold" })
 	end,
 }
